65e51881133cd5efde7dc45d976bfb4aeb144239
Vladimir Tsymbaliuk <r5ichard55@gmail.com>
2022-02-05 17:14:15 +0200

    1

---
+++ js/task-01.js
@@ -0,0 +1,33 @@
+const liEl = document.querySelectorAll(".item");
+// console.log(liEl);
+//  Number of Categories
+const numberOfCategories = liEl.length;
+console.log("Number of categories:",numberOfCategories);
+// Name & Number of Categories
+// const navEl = document.querySelector("#categories")
+// console.log(navEl);
+
+// const firstItem = navEl.firstElementChild;
+// console.log(firstItem);
+// const title = firstItem.querySelector('h2');
+// console.log("Category:", title.textContent);
+// const firstElements = firstItem.querySelectorAll("li");
+// const firstElem = firstElements.length;
+// console.log("Elements:", firstElem);
+
+// const secondItem = firstItem.nextElementSibling;
+// console.log(secondItem);
+// const secondTitle = secondItem.querySelector('h2');
+// console.log("Category:", secondTitle.textContent);
+
+// const thirdItem = navEl.lastElementChild;
+// console.log(thirdItem);
+// const thirdTitle = thirdItem.querySelector('h2');
+// console.log("Category:", thirdTitle.textContent);
+
+liEl.forEach(element => {
+	const categories = element.querySelector('h2').textContent;
+	console.log("Category:",categories);
+	const  numberOfElements = element.querySelectorAll('li').length;
+	console.log("Elements:", numberOfElements);
+});
