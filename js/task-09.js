e997647832da123d2ff030099d40e12f7a5bca77
Vladimir Tsymbaliuk <r5ichard55@gmail.com>
2022-02-07 20:57:24 +0200

    8

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColor = document.querySelector('.change-color');
+// console.dir(changeColor.style);
+const currentColor = document.querySelector('.color');
+// console.log(currentColor.value);
+const bodyColor = document.querySelector('body');
+// console.log(bodyColor.style.backgroundColor);
+
+const magicBackgroundColor = event => {
+	// console.log(bodyColor.style.backgroundColor);
+	// console.log(bodyColor.style);
+	bodyColor.style.backgroundColor = getRandomHexColor();
+	currentColor.textContent = getRandomHexColor();
+}
+
+changeColor.addEventListener('click', magicBackgroundColor)
