b4c2aad112f3f23b694ced582f7c6ffe22bc0e64
Vladimir Tsymbaliuk <r5ichard55@gmail.com>
2022-02-07 12:31:08 +0200

    6

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,14 @@ console.log(validationLength);
 console.log(validation);
 
 const checkLength = event => {
-	console.log(validation.value.length);
-console.log(validationLength.dataset.length);
-	if (validation.value.length  !== validationLength.dataset.length) {
+	console.log(Number(validation.value.length));
+console.log(Number(validationLength.dataset.length));
+	if (validation.value.length  === Number(validationLength.dataset.length)) {
 		validation.classList.add("valid")
+		validation.classList.remove("invalid")
 	} else {
 		validation.classList.add("invalid")
+		validation.classList.remove("valid")
 	}
 	 
 };
