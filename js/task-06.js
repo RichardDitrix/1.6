c3acadde4d75a9d3fc819b1186677efa418454b8
Vladimir Tsymbaliuk <r5ichard55@gmail.com>
2022-02-07 19:00:19 +0200

    8

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const validation = document.querySelector("#validation-input");
 const validationLength = document.querySelector('[data-length="6"]');
 const validInput = document.querySelector
-console.log(validationLength);
-console.log(validation);
+// console.log(validationLength);
+// console.log(validation);
 
 const checkLength = event => {
-	console.log(Number(validation.value.length));
-console.log(Number(validationLength.dataset.length));
+// 	console.log(Number(validation.value.length));
+// console.log(Number(validationLength.dataset.length));
 	if (validation.value.length  === Number(validationLength.dataset.length)) {
 		validation.classList.add("valid")
 		validation.classList.remove("invalid")
@@ -16,7 +16,7 @@ console.log(Number(validationLength.dataset.length));
 	}
 	 
 };
-console.log (checkLength);
+// console.log (checkLength);
 validation.addEventListener("blur", checkLength);
-console.log(validation.value.length);
-console.log(validationLength.dataset.length);
+// console.log(validation.value.length);
+// console.log(validationLength.dataset.length);
